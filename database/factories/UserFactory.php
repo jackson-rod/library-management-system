d41973911cef51c2214525a7f42a96ec5a29fbad
Jackson Rodriguez <jackson.rodriguezf@gmail.com>
2025-11-08 22:09:13 -0500

    feat: factory and seeders added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'User',
+            'library_id' => strtoupper($this->faker->unique()->bothify('LIB-####')),
             'remember_token' => Str::random(10),
         ];
     }
